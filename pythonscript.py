31e25bf0b8758d89ebf5a5b5513a01a8034c0d4f
ogunrindekirk <kogunrinde@smu.edu>
2023-06-05 14:01:00 -0500

    "Hello World {INT}" each line, Terminal arg Parse

--- pythonscript.py
+++ pythonscript.py
@@ -1,8 +1,26 @@
-num = int(input("Please Enter Number: "))
-msg = "Hello World {"
-print(msg, end="")
+#created by kirk ogunrinde on Jun 5 2023
 
-for i in range(num):
-    print(str(i), end="")
+#import argparse library
+import argparse
+
+#function calc that does the counting down of the "Hello World {INT}" based on parameter x
+def calc(x):
+    msg = "Hello World {"
     
-print (" }")
+    for i in range (x + 1):
+        print(msg + str(i) + "} ")
+        
+#this is technically the "main" function, renamed accrodingly so we can use argparse to enter commands in the terminal
+def main(num):
+    rem = num - 1
+    calc(rem)
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description = "Use Command Line Arguments For function")
+    parser.add_argument("main", type=int, help="Enter an Integer number")
+    args = parser.parse_args()
+    
+    num = args.main
+    main(num)
+    
+    
